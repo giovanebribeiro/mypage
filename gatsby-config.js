3641e3590da1aa484c1f5442a6cc0aca14f03dac
Giovane Boaviagem <giovane.ribeiro@neurotech.com.br>
2024-05-27 20:39:09 -0300

    fix: update gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "UA-22546389-1"
+          "G-8THB84MESS"
           /*"GA-TRACKING_ID", // Google Analytics / GA
           "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)*/
