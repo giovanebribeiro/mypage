a15a378660269c7e981e1beae8253e572a1ef94a
Giovane Boaviagem Ribeiro <giovanebribeiro@gmail.com>
2025-12-08 13:23:04 -0300

    add analytics id

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -4,7 +4,7 @@ export const initGA = () => {
   // TODO: Replace with your actual GA4 tracking ID
   // You need to create a GA4 property at https://analytics.google.com
   // Current UA code (UA-22546389-1) is deprecated and won't work with GA4
-  ReactGA.initialize('G-XXXXXXXXXX');
+  ReactGA.initialize('G-443168392');
 };
 
 export const logPageView = () => {
